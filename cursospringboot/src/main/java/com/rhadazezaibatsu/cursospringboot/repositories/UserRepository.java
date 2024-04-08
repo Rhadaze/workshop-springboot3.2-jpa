eece7bad16038112ff8b6716c8ecc721ef4d684e
Rhadaze <carlos_avilla@hotmail.com>
2024-04-08 14:15:51 -0400

    Service layer, component registration

--- cursospringboot/src/main/java/com/rhadazezaibatsu/cursospringboot/repositories/UserRepository.java
+++ cursospringboot/src/main/java/com/rhadazezaibatsu/cursospringboot/repositories/UserRepository.java
@@ -1,8 +1,11 @@
 package com.rhadazezaibatsu.cursospringboot.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.rhadazezaibatsu.cursospringboot.entities.User;
 
+@Repository //Comando opcional
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
