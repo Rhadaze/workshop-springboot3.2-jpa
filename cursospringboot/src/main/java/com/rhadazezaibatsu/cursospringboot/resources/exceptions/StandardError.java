43876530c1dff84c9e336079e1a44084bb4eb1da
Rhadaze <carlos_avilla@hotmail.com>
2024-09-25 16:34:35 -0400

    Exception handling - delete

--- cursospringboot/src/main/java/com/rhadazezaibatsu/cursospringboot/resources/exceptions/StandardError.java
+++ cursospringboot/src/main/java/com/rhadazezaibatsu/cursospringboot/resources/exceptions/StandardError.java
@@ -3,10 +3,13 @@ package com.rhadazezaibatsu.cursospringboot.resources.exceptions;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class StandardError implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-mm-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant timestamp;
 	private Integer status;
 	private String error;
@@ -18,7 +21,7 @@ public class StandardError implements Serializable {
 	public StandardError(Instant timestamp, Integer status, String error, String message, String path) {
 		super();
 		this.timestamp = timestamp;
-		this.status = status;
+		this.status = status;	
 		this.error = error;
 		this.message = message;
 		this.path = path;
--- cursospringboot/src/main/java/com/rhadazezaibatsu/cursospringboot/services/UserService.java
+++ cursospringboot/src/main/java/com/rhadazezaibatsu/cursospringboot/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.rhadazezaibatsu.cursospringboot.entities.User;
 import com.rhadazezaibatsu.cursospringboot.repositories.UserRepository;
+import com.rhadazezaibatsu.cursospringboot.services.exceptions.DatabaseException;
 import com.rhadazezaibatsu.cursospringboot.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,17 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			if (!repository.existsById(id)) {
+				throw new ResourceNotFoundException(id);
+			}
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public User update(Long id, User obj) {
